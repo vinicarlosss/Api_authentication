8013784b7ff6773ab8a8093281b058f40c901f96
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-10-06 07:02:29 -0300

    criado servidor com express

--- aulaOne/src/index.ts
+++ aulaOne/src/index.ts
@@ -1,2 +1,12 @@
+import express, {Request, Response, NextFunction} from 'express';
 
-console.log('Executei!');
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
+    res.status(200).send({ foo: 'bar' });
+});
+
+app.listen(3000, () =>{
+    console.log('Aplicação executando na porta 3000!');
+})
+
