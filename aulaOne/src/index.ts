b4c01ae2bd4f6236cad1517e06513b90531d99b9
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-10-06 08:00:33 -0300

    adicionados outros metodos de endpoints da aplicação

--- aulaOne/src/index.ts
+++ aulaOne/src/index.ts
@@ -1,14 +1,21 @@
+import exp from 'constants';
 import express, {Request, Response, NextFunction} from 'express';
 import usersRoute from './routes/users.route';
 
 const app = express();
 
+//Configurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Configurações de Rotas
 app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) =>{
     res.status(200).send({ foo: 'Sucesso ' });
 });
 
+//Inicialização do servidor
 app.listen(3000, () =>{
     console.log('Aplicação executando na porta 3000!');
 })
--- aulaOne/src/routes/users.route.ts
+++ aulaOne/src/routes/users.route.ts
@@ -1,15 +1,30 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
     const users = [{userName: 'Renan'}];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 });
 
-usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction) =>{
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) =>{
     const uuid = req.params.uuid;
-    res.status(200).send({ uuid });
+    res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) =>{
+    const newUser = req.body;
+    res.status(StatusCodes.CREATED).send(newUser)
+});
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) =>{
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) =>{
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;
