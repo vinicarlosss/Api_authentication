bc6d485f98bd846899bcbc826b2d4c2f49ac551b
Carlos Vinícius <carlos.viniciusufrpe@gmail.com>
2021-10-06 07:43:54 -0300

    adicionado metodo get com parametros

--- aulaOne/src/routes/users.route.ts
+++ aulaOne/src/routes/users.route.ts
@@ -2,9 +2,14 @@ import { Router, Request, Response, NextFunction } from "express";
 
 const usersRoute = Router();
 
-usersRoute.get('/', (req: Request, res: Response, next: NextFunction) =>{
+usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) =>{
     const users = [{userName: 'Renan'}];
     res.status(200).send(users);
+});
+
+usersRoute.get('/users/:uuid', (req: Request, res: Response, next: NextFunction) =>{
+    const uuid = req.params.uuid;
+    res.status(200).send({ uuid });
 });
 
 export default usersRoute;
